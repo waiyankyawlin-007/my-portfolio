fb42e63c3d7421f0e3baf3be60500fac8d36c00f
Wai Yan Kyaw Lin <waiyankyaw.lin@mo.com.mm>
2024-06-08 17:59:52 +0700

    commit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -23,27 +23,33 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
+  img,
+  imgclassName,
+  titleClassName,
+  spareImg,
   id,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
-  id:  number,
+  img?: string;
+  imgclassName?: string;
+  titleClassName?: string;
+  spareImg?: string
+  id?: number,
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: 'rgb(4,7,29)',
+        backgroundColor: 'gradient...'
+      }}
     >
-      {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
